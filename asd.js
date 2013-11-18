60560fff297d7577a47a51b960cd028841b31b7c
François Frisch <noreply>
2013-11-18 01:22:06 -0800

    Add module asd

---
+++ asd.js
@@ -0,0 +1,16 @@
+/**
+ * @module asd
+ * @requires montage/core/core
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Asd
+ * @extends Montage
+ */
+exports.Asd = Montage.specialize(/** @lends Asd# */ {
+    constructor: {
+        value: function Asd() {
+            this.super();
+        }
+    }
+});
